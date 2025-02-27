8892812d242fdc3b13c2ad22ba12d6e7ff61dac9
taqin <cp@imtaqin.id>
2025-02-27 12:02:50 +0700

    sadasd

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -10,44 +10,7 @@ const { title, description, github } = useAppConfig()
   <main
     class="grid pt-8 pb-8 lg:grid-cols-2 place-items-center md:py-12"
   >
-    <div>
-      <h1
-        class="text-5xl font-bold lg:text-6xl xl:text-7xl lg:tracking-tight xl:tracking-tighter"
-      >
-        {{ title }}
-      </h1>
-      <p class="max-w-xl mt-4 text-lg text-slate-600">
-        {{ description }}
-      </p>
-      <div class="flex flex-col gap-3 mt-6 sm:flex-row">
-        <HomeLink
-          href="/dashboard"
-          target="_blank"
-          title="Dashboard"
-          class="flex items-center justify-center gap-1"
-          rel="noopener"
-        >
-          <AreaChart
-            class="w-5 h-5"
-          />
-          Dashboard
-        </HomeLink>
-        <HomeLink
-          size="lg"
-          type="outline"
-          rel="noopener"
-          :href="github"
-          title="Github"
-          class="flex items-center justify-center gap-1"
-          target="_blank"
-        >
-          <GitHubIcon
-            class="w-5 h-5"
-          />
-          GitHub Repo
-        </HomeLink>
-      </div>
-    </div>
+    
     <div class="hidden py-6 md:block">
       <div
         class="w-[512px]"
